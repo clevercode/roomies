2c26afe4d73f4d13afb2c967c37e181305a10f7e
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-14 15:52:31 -0400

    Created the Rails project, moved all docs to /doc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomies::Application.config.secret_token = 'b3b0f18c1f4479fb411f506fed3e8c92c1abf433014f5322dce4661bec1886035f6199a05f524c78b231288243169e61c713537cce78ebaf1937897951a55128'
