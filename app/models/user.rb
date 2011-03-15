94987c0cb0d7dafda5f38e4fca966e0af0738dba
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-15 19:32:24 -0400

    Can't do single line multi has_many's, noobs gotta noob.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  has_many :tasks, :expenses, :achievements
+  has_many :tasks
+  has_many :expenses
+  has_many :achievements
 end
