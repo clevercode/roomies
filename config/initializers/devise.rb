fcf59667515c22e68a4fe4262da32a33bcb13ede
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-15 03:52:40 -0400

    Erroneous action mailer setting in devise.rb.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -139,5 +139,4 @@ Devise.setup do |config|
   #   end
   #   manager.default_strategies(:scope => :user).unshift :twitter_oauth
   # end
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
