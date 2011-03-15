6132006dc794a67e3aa9e06c69c42832f92b18eb
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-15 06:08:43 -0400

    Routed the root to pages#home, matched about, terms and privacy to their respective Pages controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
-Roomies::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/about"
-
-  get "pages/terms"
-
-  get "pages/privacy"
+Roomies::Application.routes.draw do 
+  
+  root :to => "pages#home"
+  
+  match 'about' => 'pages#about'
+  match 'terms' => 'pages#terms'
+  match 'privacy' => 'pages#privacy'
 
   devise_for :users
 
@@ -58,7 +57,6 @@ Roomies::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
