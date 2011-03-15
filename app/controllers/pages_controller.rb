737f5efcc9a9af59129b125b448f38cff506747a
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-15 06:09:46 -0400

    Added a title function to the application helper that outputs a default long title or "short title | current page title" if @title has been set.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,15 @@ class PagesController < ApplicationController
   end
 
   def about
+    @title = "About us"
   end
 
   def terms
+    @title = "Terms of service"
   end
 
   def privacy
+    @title = "Privacy policy"
   end
 
 end
