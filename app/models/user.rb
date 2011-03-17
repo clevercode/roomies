cadc88e2c58ead06005d5fea2bd7791e3d9f327f
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-17 19:07:35 -0400

    The User model is now a proper MongoDB citizen, added a name field as well which is now required but need not be unique.
    Might have to rethink that one if we provide public facing URLs for users at a later date.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,18 +20,26 @@
 #  house_id             :integer
 #
 
-class User < ActiveRecord::Base
-  has_many :chores
-  has_many :expenses
-  has_one :house 
+class User 
+  include Mongoid::Document
+
+  # has_many :chores
+  # has_many :expenses
+  # has_one :house 
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable, :validatable
+
+  field :name
+
+  validates :name, :presence => true
+  validates :email, :presence => true,
+                    :uniqueness => true
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
   def to_s
     self.email
