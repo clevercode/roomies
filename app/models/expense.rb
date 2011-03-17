31c853c7af40e3d92118f85d35e34aa2e49ff905
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-16 20:10:53 -0400

    Added validation of presence for :user_id, tasks & expenses need assignees.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
   belongs_to :house
+
+  validates :user_id, :presence => true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :house
+
+  validates :user_id, :presence => true
 end
