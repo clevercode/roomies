c3b206de2bb5972536dba0ac0442e82f9139f24b
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-21 03:18:49 -0400

    Added the keys and secrets we received from Twitter and Facebook (created accounts on both dev centers for Roomies).
    I invited Zach and Andrew to the Facebook account for Roomies.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET' 
-  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :twitter, '6L5zgyu7vznSUPQQkyWEqg', 'pyod6U46aColNjQ2tni5y7tOF6BVIsXPh3hTow' 
+  provider :facebook, '143753272357200', '1a49da80193a3c81e1bc01b43837830e'
 end  
 
