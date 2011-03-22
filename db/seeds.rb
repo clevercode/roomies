808bf25ff0f9865cd77a9c74e66c9e6051709fcf
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-22 03:02:53 -0400

    Created a proper seed with 4 users, didn't think I would do that this early. Prouditude.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+#
+puts 'EMPTY THE MONGODB DATABASE'
+
+Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+
+user = User.create! {[
+  { :name => 'Azimuth', :email => 'azimuth@clevercode.net', :password => 'evilone', :password_confirmation => 'evilone' },
+  { :name => 'Olivier', :email => 'oli@clevercode.net', :password => 'smidge', :password_confirmation => 'smidge' },
+  { :name => 'Zach', :email => 'zach@clevercode.net', :password => 'smidge', :password_confirmation => 'smidge' },
+  { :name => 'Andrew', :email => 'andrew@clevercode.net', :password => 'smidge', :password_confirmation => 'smidge' }
+]}
+
+user.each do |u|
+  puts 'New user created: ' << u.name
+end
