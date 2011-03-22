be5cc06ba65faa30c6d3927bf062db7050d8e259
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-22 02:23:57 -0400

    Added comments and registration hooks.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -10,20 +10,33 @@ class AuthenticationsController < ApplicationController
     authentication = Authentication.where(:provider => omniauth['provider'], :uid => omniauth['uid']).first
   
     if authentication
+      # Just sign in an existing user with omniauth
+      # The user have already used this external account
       flash[:notice] = t(:signed_in)
       sign_in_and_redirect(:user, authentication.user)
 
     elsif current_user
+      # Add authentication to signed in user
+      # User is logged in      
       current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
       flash[:notice] = t(:auth_success)
-
       redirect_to authentications_url
-    elsif user = create_new_omniauth_user(omniauth)
+    
+    elsif omniauth['provider'] != 'twitter' && omniauth['provider'] != 'linked_in' && user = create_new_omniauth_user(omniauth)
       user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      flash[:notice] = t(:signed_in)
-
+      # Create a new User through omniauth
+      # Register the new user + create new authentication
+      flash[:notice] = t(:welcome)
       sign_in_and_redirect(:user, user)
+   
+    elsif (omniauth['provider'] == 'twitter' || omniauth['provider'] == 'linked_in') && 
+      omniauth['uid'] && (omniauth['user_info']['name'] || omniauth['user_info']['nickname'] || 
+      (omniauth['user_info']['first_name'] && omniauth['user_info']['last_name']))
+      session[:omniauth] = omniauth.except('extra');
+      redirect_to(:controller => 'registrations', :action => 'email')
+    
     else
+      # New user data not valid, try again
       flash[:alert] = t(:auth_fail)
       redirect_to new_user_registration_url
     end
@@ -38,7 +51,7 @@ class AuthenticationsController < ApplicationController
 
   def create_new_omniauth_user(omniauth)
     user = User.new
-    user.apply_omniauth(omniauth)
+    user.apply_omniauth(omniauth, true)
     if user.save
       user
     else
