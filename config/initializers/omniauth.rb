b8f6580f3f75aa06717d9572939c6911d5bdd745
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-22 01:23:21 -0400

    Added OpenID implementation logic and commented it. Hate OpenID, but just in case it'll be ready.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
+# require 'openid/store/filesystem'
+
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, '6L5zgyu7vznSUPQQkyWEqg', 'pyod6U46aColNjQ2tni5y7tOF6BVIsXPh3hTow' 
   provider :facebook, '143753272357200', '1a49da80193a3c81e1bc01b43837830e'
+
+  # provider :openid, OpenID::Store::Filesystem.new('/tmp')
+  # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
+  # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'  
 end  
 
