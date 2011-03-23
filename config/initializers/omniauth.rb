45d503da6cf18ae62dcbb181efa94f72a686ab9b
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-23 04:01:31 -0400

    Fixed erroneous Facebook keys.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_KEY']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 
   # provider :openid, OpenID::Store::Filesystem.new('/tmp')
   # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
