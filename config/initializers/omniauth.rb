37c6d483472d6cf16a6834cebda4e7bfcfe63a46
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-23 03:44:17 -0400

    Used environment variables for oAuth API keys.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 # require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, '6L5zgyu7vznSUPQQkyWEqg', 'pyod6U46aColNjQ2tni5y7tOF6BVIsXPh3hTow' 
-  provider :facebook, '143753272357200', '1a49da80193a3c81e1bc01b43837830e'
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_KEY']
 
   # provider :openid, OpenID::Store::Filesystem.new('/tmp')
   # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
