9ee36031a908e40ce68d51f397e5fdd32ee19722
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-23 04:24:18 -0400

    Confirmable doesn't play nice for now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable, :confirmable
+        :recoverable, :rememberable, :trackable, :validatable
 
   field :name
 
