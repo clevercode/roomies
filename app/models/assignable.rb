1be471bf3a425fab69e8cccbdfdaf8c3194ee725
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-24 21:59:55 -0400

    Added purpose, due date, recurring and references to assignees.

--- app/models/assignable.rb
+++ app/models/assignable.rb
@@ -1,3 +1,9 @@
 class Assignable
   include Mongoid::Document
+
+  field :purpose, :type => String
+  field :due_date, :type => DateTime
+  field :recurring, :type => Boolean
+
+  references_many :assignees
 end
