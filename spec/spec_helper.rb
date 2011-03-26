5d371da67c418e3e9a77acaecad33e5da482b9b2
Olivier Lacan <olivier.lacan@gmail.com>
2011-03-26 16:37:38 -0400

    Added better Mongoid integration to RSpec.
    As described here: http://mongoid.org/docs/integration/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  config.after :suite do
+    Mongoid.master.collections.select do |collection|
+      collection.name !~ /system/
+    end.each(&:drop)
+  end
+
 end
