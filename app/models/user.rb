9c94c6e3f12aa6e6e71391bf6240e0a7d7ec3c7c
Zachary Nicoll <zacharynicoll@gmail.com>
2011-03-28 19:22:43 -0400

    Removed name validation for homepage signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name,  :presence => true
   validates :email, :presence => true,
                     :uniqueness => true
 
