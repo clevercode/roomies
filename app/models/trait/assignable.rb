a56a458f219156e140b24ada7ce8826dd11027cb
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-01 05:28:18 -0400

    Added User class name for the relationship with assignees so that we don't have to create an Assignees model.

--- app/models/trait/assignable.rb
+++ app/models/trait/assignable.rb
@@ -6,7 +6,7 @@ module Trait
     included do
       # has access to an array of Assignees that have its id for assignable_id
       # also stores the references to every assignees on the Assignable module itself
-      has_and_belongs_to_many :assignees, :stored_as => :array
+      has_and_belongs_to_many :assignees, :stored_as => :array, :class_name => "User"
     end
   end
 end
