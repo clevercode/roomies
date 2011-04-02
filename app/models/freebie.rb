ca99e3ff23b8de596d67a55257559888fdf63b1d
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-02 03:54:40 -0400

    Included commissionable trait (will need the creator to be logged to avoid him from confirming his own freebie).

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,2 +1,8 @@
 class Freebie < Assignment
+  # a freebie is a voluntary action that benefits
+  # the household and for which the commissioner
+  # may claim points to be defined by the validator
+  #
+  # NB: this might be too similar to bounty
+  include Trait::Commissionable
 end
