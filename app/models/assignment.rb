629ffcc8406769e5e685b9864c34639c5dded919
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-02 03:37:59 -0400

    Added purpose field to basic Assignment model. Included Completable and Validatable traits as universal defaults.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,12 @@
 class Assignment
   include Mongoid::Document
 
+  field :purpose, :type => String
   field :value, :type => Integer
-  
+
+  include Trait::Completable
+  include Trait::Validatable
+
   belongs_to :house
   belongs_to :category
 end
