5eb2d4da7b420b2a563ffe352ae18facd0190a9f
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-04 17:06:01 -0400

    Added validator for name to the house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,17 +1,19 @@
 class House
   include Mongoid::Document
   
-  validates_uniqueness_of :name, :message => "Sorry! House already exists!"
-  
   # Fields
   field :name, :type => String
+  
+  validates :name, :presence => true
+  
+  attr_accessible :name
 
   # Associations
   has_many :users # has access to an array of Users that have its id for house_id
   has_many :expenses, :through => :users
-  has_many :tascs, :through => :users
-  has_many :bills, :through => :users
-  has_many :chores, :through => :users
+  has_many :tascs,    :through => :users
+  has_many :bills,    :through => :users
+  has_many :chores,   :through => :users
   has_many :freebies, :through => :users
   has_many :bounties, :through => :users
 end
