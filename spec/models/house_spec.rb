5b790d7a3ce593b95c62cc33b034a9d5d7a43e23
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-04 17:07:14 -0400

    Began writing user and house testing scripts.

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe House do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { 
+      :name => "House"
+    }
+  end
+  
+  it "should create a new house given valid attributes" do
+    house = House.new(@attr)
+    house.should be_valid
+  end
+  
+  it "should require a name to be created" do
+    house = House.new(@attr.merge(:name => ""))
+    house.should_not be_valid
+  end
+  
+  it "should accept duplicate names" do
+    House.create!(@attr)
+    house = House.new(@attr)
+    house.should be_valid
+  end
+  
+  it "should have access to any users who belong to it" do
+    house = House.new(@attr)
+    house.should respond_to(:users)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,84 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { 
+      :name => "Andrew Smith",
+      :email => "ae@smith.com",
+      :password => "muffins",
+      :password_confirmation => "muffins"
+    }
+  end
+
+  it "should create a new user given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should not require a name" do
+    user = User.new(@attr.merge(:name => ""))
+    user.should be_valid
+  end
+
+  it "should require an email address" do
+    user = User.new(@attr.merge(:email => ""))
+    user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@email.com THE_USER@e.mail.org first.last@something.pu]
+    addresses.each do |address|
+      user = User.new(@attr.merge(:email => address))
+      user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@email,com user_at_email.org first.last@email.]
+    addresses.each do |address|
+      user = User.new(@attr.merge(:email => address))
+      user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user = User.new(@attr)
+    user.should_not be_valid
+  end
+
+  it "should reject email addresses identical to upcase" do
+    email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => email))
+    user = User.new(@attr)
+    user.should_not be_valid
+  end
+
+  describe "passwords" do
+    before(:each) do
+      @user = User.new(@attr)
+    end
+
+    it "should not accept passwords shorter than 6 characters" do
+      pass = 'z' * 5
+      user = User.new(@attr.merge(:password => pass))
+      user.should_not be_valid
+    end
+    
+    it "should not accept passwords longer than 20 characters" do
+      pass = 'z' * 21
+      user = User.new(@attr.merge(:password => pass))
+      user.should_not be_valid
+    end
+    
+    # it "should not require a password" do
+    #   user = User.new(@attr.merge(:password => ""))
+    #   user.should be_valid
+    # end
+  end
+  
+  it "should have access to the house it belongs to" do
+    house = House.new(:name => "House")
+    user = User.new(@attr.merge(:house => @house))
+    user.should respond_to(:house)
+  end
 end
