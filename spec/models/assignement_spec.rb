9a918c1c5aa8d076cd809f71c2f9c01f041f7ac5
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-04 17:04:35 -0400

    Moved some files around, and removed some files that were breaking the test scripts. We'll find out if that was a good thing to do or not pretty soon.

--- spec/models/assignement_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Assignement do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/assignments/show.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "assignments/show.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
