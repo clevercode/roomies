84041e0ce38949673320e6225f0599c63749c811
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-05 13:43:25 -0400

    Removed tasks and expenses from routed resources, using assignments instead.

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Roomies::Application.routes.draw do
   resources :users
   resources :authentications
   resources :achievements
-  resources :expenses
-  resources :tascs
   resources :houses
   resources :corkboard
+  resources :assignments
 
   match '/auth/:provider/callback' => 'authentications#create'
   match '/registrations' => 'registrations#email'
   match '/user/:id/homeless/:house_id' => 'houses#destroy_roomie', :as => :homeless
-  match '/tasks(/:action)' => 'tascs#index'
 
   root :to => 'home#index'
   namespace :user do
@@ -67,10 +65,6 @@ Roomies::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
