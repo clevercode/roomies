e063397d9dbd813da921774525b18faba5051011
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-13 08:16:55 -0400

    We're not using ActiveRecord anymore, these files are useless.

--- db/migrate/20110316231937_add_house_id_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddHouseIdToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :house_id, :integer
-  end
-
-  def self.down
-    remove_column :users, :house_id
-  end
-end
