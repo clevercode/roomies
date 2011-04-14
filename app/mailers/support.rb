2d24e61d6b1f848322f30c8e3c918900e6ea3235
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-14 19:08:56 -0400

    Added a message and a reply to to the support email headers

--- app/mailers/support.rb
+++ app/mailers/support.rb
@@ -2,12 +2,19 @@ class Support < ActionMailer::Base
   default :from => "support@roomiesapp.com"
   
   def submit_request(params)
-    mail(:to      => 'zacharynicoll@gmail.com',
-         :from    => 'support@roomiesapp.com',
-         :subject => params[:request][:message].split[0..7].join(' '),
-         :title   => "A message from #{params[:request][:name]}:",
-         :email   => params[:request][:email],
-         :message => params[:request][:message]
-    )
+    request = params[:request]
+    mail(:to       => 'contact@clevercode.net',
+         :reply_to => request[:email],
+         :subject  => request[:message].split[0..7].join(' '),
+         :title    => "A message from #{request[:name]}:",
+         :email    => request[:email]
+    ) do |format|
+      format.text { render :text  => "#{request[:message]}" }
+      format.html { render :text  => "<h1>Hey there, you sexy beast!</h1>" +
+                                     "<h3>It seems you've gotten a new message from a loyal follower at roomies. Yay for you!</h3>" +
+                                     "===================================" +
+                                     "<p>#{request[:message]}</p>"
+      }
+    end
   end
 end
