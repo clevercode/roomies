5d382d821fe482edab23ca8d846d3d5b0fc58b11
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-13 23:46:54 -0400

    Creating simple mail object to be used as a template when sending support requests.

---
+++ app/mailers/support.rb
@@ -0,0 +1,13 @@
+class Support < ActionMailer::Base
+  default :from => "support@roomiesapp.com"
+  
+  def submit_request(params)
+    mail(:to      => 'zacharynicoll@gmail.com',
+         :from    => params[:request][:email],
+         :subject => params[:request][:message].split[0..7].join(' '),
+         :title   => "A message from #{params[:request][:name]}:",
+         :email   => params[:request][:email],
+         :message => params[:request][:message]
+    )
+  end
+end
