d74e58491a9bd3a705669fb684f7d9c51262fc0d
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-15 18:45:44 -0400

    This thoroughly works now that the typos were busted by Zach's flair.

--- app/models/assignment_factory.rb
+++ app/models/assignment_factory.rb
@@ -1,23 +1,28 @@
 class AssignmentFactory
 
-  def self.new(attributes)
+  def self.new(attributes = {})
 
     # if there are no assignees
     if attributes[:assignees].blank?
 
       # bounties, freebies, gifts & wishes
       if attributes[:cost].nil?
-        if attributes[:comissionned_at].nil?
+        if attributes[:commissioned_at].nil?
           Freebie.new(attributes)
         else
           Bounty.new(attributes)
         end
       else
+        if attributes[:completed_at].nil?
+          Wish.new(attributes)
+        else
+          Gift.new(attributes)
+        end
       end
 
     # if there are assignees
     else
-      if attributes[:cost].blank?
+      if attributes[:cost].nil?
         # tasc or chore
         if attributes[:frequency].blank?
           Tasc.new(attributes)
