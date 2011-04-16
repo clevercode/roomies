0001dbda480d130adfc915ddcae863258c64265b
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-16 03:51:11 -0400

    Created instance variables for all assignments types and one big one joining tasks, expenses, bills and chores.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -1,5 +1,18 @@
 class CorkboardController < ApplicationController
   def index
     @user = current_user
+    @freebies = Freebie.all
+    @bounties = Bounty.all
+    @gifts = Gift.all
+    @wishes = Wish.all
+
+    @tasks = Tasc.all
+    @expenses = Expense.all
+    @bills = Bill.all
+    @chores = Chore.all
+
+    @things = []
+    @things = @tasks + 
+              @expenses
   end
 end
