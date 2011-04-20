a51ba37a034347387a2de1d483b33300a7d741ed
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-20 09:23:54 -0400

    Added default text for welcome email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ActionMailer::Base
   default :from => "notifications@roomiesapp.com"
   
-  def welcome_email(user)
-    @user = user
-    @url  = "http://example.com/login"
-    mail(:to => user.email,
-         :subject => "Welcome to My Awesome Site")
+  def welcome_email(params)
+    mail(:to      => params[:email],
+         :subject => "Welcome to Roomies!",
+    ) do |format|
+      format.text { render :text  => "You should have html emails enabled dummy." }
+      format.html { render :text  => "<h1>Hey there, you sexy beast!</h1>" +
+                                    "===================================" +
+                                    "<p>You have successfully signed up to roomie and we love you.</p>" +
+                                    "<p>Thanks for joining and have a great day!</p>"
+      }
+    end
   end
 end
