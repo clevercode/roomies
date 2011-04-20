61e07362cd56c15df56a77cc767e923ab7098eaa
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-20 14:33:58 -0400

    Added the invitation_email to the user_mailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,27 @@ class UserMailer < ActionMailer::Base
       format.text { render :text  => "You should have html emails enabled dummy." }
       format.html { render :text  => "<h1>Hey there, you sexy beast!</h1>" +
                                     "===================================" +
-                                    "<p>You have successfully signed up to roomie and we love you.</p>" +
-                                    "<p>Thanks for joining and have a great day!</p>"
+                                    "<p>You have successfully signed up to Roomies and we" +
+                                    "love you. Thanks for joining and have a great day!</p>"
+      }
+    end
+  end
+
+  def invitation_email(params)
+    mail(:to      => params[:email],
+         :subject => "You have a new invitation from a friend at Roomies!",
+    ) do |format|
+      format.text { render :text  => "You should have html emails enabled dummy." }
+      format.html { render :text  => "<h1>Hey there, you sexy beast!</h1>" +
+                                    "===================================" +
+                                    "<p>Someone you live with(hopefully), sent you an " +
+                                    "invitation to move into their house on Roomies!</p>" +
+                                    "<p>If you already have an account, you can simple sign " +
+                                    "in and accept the invitation. If you're new to the whole " +
+                                    "thing, head over to roomiesapp.com and signup. As soon " +
+                                    "as your logged in, you will see a notification staring you " +
+                                    "in the face, asking you to accept or reject the invitation. " +
+                                    "Have super fantabulous day!</p>"
       }
     end
   end
