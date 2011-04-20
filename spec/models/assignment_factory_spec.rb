bc10f85fddb0a6484113a7870eb2f9887fc613da
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-20 02:47:56 -0400

    Eight beautifully successful tests for basic assignment differentiation based on data entered by users.

--- spec/models/assignment_factory_spec.rb
+++ spec/models/assignment_factory_spec.rb
@@ -10,30 +10,86 @@ describe Assignment do
 
     @params = {
       :purpose => 'Find the meaning of life',
-      :category => 'General',
       :due_at => 'Thu, 24 Apr 2011 19:18:59 -0400',
       :commissioned_at => 'Thu, 14 Apr 2011 19:18:59 -0400',
-      :validated_at => '',
       :assignees => [@user.id],
-      :repeating => 'no',
-      :frequency => '',
-      :cost => 2
+      :frequency => 10,
+      :cost => 1
     }
 
-    @assignment = AssignmentFactory.new(@params)
+    # @assignment = AssignmentFactory.new(@params)
   end
 
-  it 'should create a new assignement with valid parameters' do
-    @assignment.should be_valid
+  it 'should create a task given specific data' do
+    assignment = AssignmentFactory.new(
+      @params.merge(
+        :cost => nil, 
+        :frequency => nil
+      )
+    )
+    assignment.should be_valid
+    assignment.should be_a(Tasc)
   end
 
-  it 'should not work with no paramters' do
-    @empty_params = {}
-    @empty_assignment = AssignmentFactory.new(@empty_params)
-    @empty_assignment.should_not be_valid
+  it 'should create a chore given specific data' do
+    assignment = AssignmentFactory.new(
+      @params.merge(:cost => nil)
+    )
+    assignment.should be_valid
+    assignment.should be_a(Chore)
   end
 
-  it 'should be an expense' do
-    @assignment.should be_a Expense
+  it 'should create an expense given specific data' do
+    assignment = AssignmentFactory.new(
+      @params.merge(:frequency => nil)
+    )
+    assignment.should be_valid
+    assignment.should be_a(Expense)
   end
+
+  it 'should create a bill given specific data' do
+    assignment = AssignmentFactory.new(@params)
+    assignment.should be_valid
+    assignment.should be_a(Bill)
+  end
+
+  it 'should create a freebie given specific data' do
+    assignment = AssignmentFactory.new({
+      :purpose => "Did something nice",
+      :claimed_at => DateTime.new,
+      :claimant_id => @user.id
+    })
+    assignment.should be_valid
+    assignment.should be_a(Freebie)
+  end
+
+  it 'should create a bounty given specific data' do
+    assignment = AssignmentFactory.new({
+      :purpose => "We should fix this someday",
+    })
+    assignment.should be_valid
+    assignment.should be_a(Bounty)
+  end
+
+  it 'should create a Gift given specific data' do
+    assignment = AssignmentFactory.new({
+      :purpose => "Hey I bought you something",
+      :cost => 2,
+      :claimed_at => DateTime.new,
+      :claimant_id => @user.id
+    })
+    assignment.should be_valid
+    assignment.should be_a(Gift)
+  end
+
+  it 'should create a Wish given specific data' do
+    assignment = AssignmentFactory.new({
+      :purpose => "We should buy this someday",
+      :cost => 2
+    })
+    assignment.should be_valid
+    assignment.should be_a(Wish)
+  end
+
+
 end
