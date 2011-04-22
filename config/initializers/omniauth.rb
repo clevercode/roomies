8b3e06238b6bef6a756dea2582b8dc8faf9622dd
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-22 03:11:27 -0400

    Prepared the OmniAuth configuration for Google authentication.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,14 @@
-# require 'openid/store/filesystem'
+require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 
-  # provider :openid, OpenID::Store::Filesystem.new('/tmp')
-  # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
-  # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'  
-end  
+  # generic openid
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
 
+  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  # provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
+  # /auth/google_apps; you can bypass the prompt for the domain with /auth/google_apps?domain=somedomain.com
+
+end
