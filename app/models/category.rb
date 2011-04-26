03b3f20210a7ad8fe357354615cdeb610249cd33
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-26 03:17:36 -0400

    Added a category_name getter/setter that returns the category.name field or creates one if one doesn't exist with the same name.

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,4 +7,12 @@ class Category
   # Associations
   has_many :assignables
 
+  def category_name
+    category.name if category
+  end
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name) unless name.blank?
+  end
+
 end
