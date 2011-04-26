03a8a9a9c963a91b0cd94575b49223bba87ef1b2
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-26 03:16:52 -0400

    Merged all fields and associations from previous data structure into Assignment.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,42 +1,31 @@
 class Assignment
-  # basic model for assignements to be
-  # inherited by more specific models
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :purpose, :type => String
+  field :due_date, :type => Date
+  field :frequency, :type => Integer
+  field :cost, :type => Float  
+
+  field :refused_at, :type => DateTime
+  field :completed_at, :type => DateTime
+  field :validated_at, :type => DateTime
+
+  # can be added on completion or refusal
+  field :comments, :type => String
+  
+
+
+  # achievement
   field :value, :type => Integer
 
-  include Trait::Completable
-  include Trait::Validatable
-
-  belongs_to :house
+  belongs_to :house, :dependent => :delete
   belongs_to :category
 
-  validates :purpose, :presence => true
+  has_and_belongs_to_many :assignees, :stored_as => :array, :class_name => "User"
+  belongs_to :commissioner, :class_name => "User"
+  belongs_to :validator, :class_name => "User"
 
-  # Returns an instance of the specified class with the attributes
-  # and errors of the current document.
-  #
-  # @example Return a subclass document as a superclass instance.
-  #   manager.becomes(Person)
-  #
-  # @raise [ ArgumentError ] If the class doesn't include Mongoid::Document
-  #
-  # @param [ Class ] klass The class to become.
-  #
-  # @return [ Document ] An instance of the specified class.
-  def becomes(klass)
-    unless klass.include?(Mongoid::Document)
-      raise ArgumentError, 'A class which includes Mongoid::Document is expected'
-    end
-    became = klass.new
-    became.instance_variable_set('@attributes', @attributes)
-    became.instance_variable_set('@errors', @errors)
-    became.instance_variable_set('@new_record', new_record?)
-    became.instance_variable_set('@destroyed', destroyed?)
-    became
-  end
-  
+  validates :purpose, :presence => true
 
 end
