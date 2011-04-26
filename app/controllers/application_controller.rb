1fab75c5a38c3344f84d5c6648881cd0c26f80d9
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-26 04:17:58 -0400

    Figured out how to properly use after_sign_in_path_for(user), going to corkboard_index_url now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :prepare_sign_in
 
+  protected
+  def after_sign_in_path_for(user)
+    puts "called from after_sign_in_path"
+    corkboard_index_url
+  end
+  
+
   private
   def prepare_sign_in
     @user = User.new
