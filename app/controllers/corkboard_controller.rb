d7d5c1871b92b6398f16fa87e307539d5df7a351
Zachary Nicoll <zacharynicoll@gmail.com>
2011-04-26 13:08:06 -0500

    Added conditional to check if you're signed in or not when accessing the corkboard.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -1,8 +1,12 @@
 class CorkboardController < ApplicationController
   def index
-    @user = current_user
-    @todays_assignments = Assignment.where(:due_date => Date.today.to_s)
-    @tomorrows_assignments = Assignment.where(:due_date => Date.tomorrow.to_s)
-    @next_days_assignments = Assignment.where(:due_date => Date.tomorrow.tomorrow.to_s)
+    if signed_in?
+      @user = current_user
+      @todays_assignments = Assignment.where(:due_date => Date.today.to_s)
+      @tomorrows_assignments = Assignment.where(:due_date => Date.tomorrow.to_s)
+      @next_days_assignments = Assignment.where(:due_date => Date.tomorrow.tomorrow.to_s)
+    else
+      redirect_to '/users/sign_in', :notice => 'Sorry, you need to sign in before viewing that page.'
+    end
   end
 end
