fe479279090e11b88da7d01136fa28112be39b1a
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-28 07:19:04 -0400

    Checks URL for locale setting (e.g. root/en/controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :prepare_sign_in
 
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+
   protected
   def after_sign_in_path_for(user)
     puts "called from after_sign_in_path"
