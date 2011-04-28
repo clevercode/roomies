78bbfebf33d6c7dac780806ca0d4def7d8e0c458
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-28 01:00:38 -0400

    Setup the Reward model with fields and associations.

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,18 @@
 class Reward
   include Mongoid::Document
+  include Mongoid::Timestamps::Created
+
+  field :type, :type => Symbol, :default => :random
+  field :points, :type => Integer, :default => 1
+
+  belongs_to :user
+
+  TYPES = {
+    :random => { :points => 0 },
+    :sign_in => { :points => 1 },
+    :assignment => { :points => 2 },
+    :completion => { :points => 3 },
+    :validation => { :points => 1 }
+  }
+
 end
