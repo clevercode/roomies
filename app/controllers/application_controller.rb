9cb5e0eeeb3cdd438c0e5486b777114b31ea4f52
Olivier Lacan <olivier.lacan@gmail.com>
2011-04-28 02:39:05 -0400

    Created a reward(type, points) protected function on the application controller to wrap Reward.create()
    Defaulting type to :controller_name_action_name which is pretty nifty if I say so myself.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,20 @@ class ApplicationController < ActionController::Base
   protected
   def after_sign_in_path_for(user)
     puts "called from after_sign_in_path"
+    reward(nil, 1)
     corkboard_index_url
   end
   
+  def reward(type, points)
+    type ||= (self.controller_name + "_" + self.action_name).to_sym
+    puts type
+    Reward.create( 
+                  :user => current_user, 
+                  :type => type,
+                  :points => points
+    )
+    flash[:notice] = "Hey look, you just got a reward!"
+  end
 
   private
   def prepare_sign_in
