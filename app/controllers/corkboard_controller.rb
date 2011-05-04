7824b096668efa07b007f21fb1f42ffe73f42bcc
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-04 04:19:56 -0400

    Hum, why are we doing conditional checks on whether a user is signed in when we have specific Devise before filters to do that? /cc @turtleguyy

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -1,22 +1,20 @@
 class CorkboardController < ApplicationController
-  def index
-    if signed_in?
-      past_assignments = []
-      Assignment.where(:completed_at => nil).map do |assignment|
-        if assignment.due_date < Date.today
-          past_assignments.push assignment
-        end
-      end
 
-      @user = current_user
-      @todays_assignments = Assignment.where(due_date: Date.today.to_s)
-      @tomorrows_assignments = Assignment.where(due_date: Date.tomorrow.to_s)
-      @next_days_assignments = Assignment.where(due_date: Date.tomorrow.tomorrow.to_s)
-      @all_tasks = Assignment.where(house_id: current_user.house.id)
-      @my_tasks = @all_tasks.where(assignee_ids: [current_user.id])
-      @past_assignments = past_assignments
-    else
-      redirect_to '/users/sign_in', :notice => 'Sorry, you need to sign in before viewing that page.'
+  before_filter :authenticate_user!
+  def index
+    past_assignments = []
+    Assignment.where(:completed_at => nil).map do |assignment|
+      if assignment.due_date > Date.today
+        past_assignments << assignment
+      end
     end
+
+    @user = current_user
+    @todays_assignments = Assignment.where(due_date: Date.today.to_s)
+    @tomorrows_assignments = Assignment.where(due_date: Date.tomorrow.to_s)
+    @next_days_assignments = Assignment.where(due_date: Date.tomorrow.tomorrow.to_s)
+    @all_tasks = Assignment.where(house_id: current_user.house.id)
+    @my_tasks = @all_tasks.where(assignee_ids: [current_user.id])
+    @past_assignments = past_assignments
   end
 end
