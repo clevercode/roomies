4f166e0c720f1fdec8e71ca264b09b03f9071b14
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-04 03:51:37 -0400

    Created instance variables for all tasks, and the current user's tasks (@my_tasks).

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -2,9 +2,11 @@ class CorkboardController < ApplicationController
   def index
     if signed_in?
       @user = current_user
-      @todays_assignments = Assignment.where(:due_date => Date.today.to_s)
-      @tomorrows_assignments = Assignment.where(:due_date => Date.tomorrow.to_s)
-      @next_days_assignments = Assignment.where(:due_date => Date.tomorrow.tomorrow.to_s)
+      @todays_assignments = Assignment.where(due_date: Date.today.to_s)
+      @tomorrows_assignments = Assignment.where(due_date: Date.tomorrow.to_s)
+      @next_days_assignments = Assignment.where(due_date: Date.tomorrow.tomorrow.to_s)
+      @all_tasks = Assignment.where(house_id: current_user.house.id)
+      @my_tasks = @all_tasks.where(assignee_ids: [current_user.id])
     else
       redirect_to '/users/sign_in', :notice => 'Sorry, you need to sign in before viewing that page.'
     end
