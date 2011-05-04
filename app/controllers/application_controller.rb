6b68ceb553bd9350a2d773a7783172f688f9cf86
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-04 15:30:55 -0400

    Still not quite sure exactly how locales get set, but it works with URL parameters. Next, we need to add user settings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :prepare_sign_in
+  before_filter :prepare_sign_in, :set_locale
+
+  def set_locale
+    logger.debug "set_locale is passed options: #{params[:locale]}\n"
+    # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = params[:locale]
+  end
+    
 
   def default_url_options(options={})
+    I18n.locale = params[:locale]
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
