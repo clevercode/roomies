a6e3e941688e44d1fcae0e8c3f00c9cde18a9f46
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-04 06:15:53 -0400

    Added scoped instance variables for @tasks & @expenses.
    Renamed, reorganized and rewrote all assignment-related instance variables in Corkboard controller to use the shorter hasrocket-less Mongoid query syntax. Removed repetitions by reusing variables.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -2,6 +2,16 @@ class CorkboardController < ApplicationController
 
   before_filter :authenticate_user!
   def index
+
+    @all = Assignment.where(house_id: current_user.house.id)
+    @my = @all.where(assignee_ids: [current_user.id])
+    @tasks = @all.where(type: "task")
+    @expenses = @all.where(type: "expense")
+
+    @todays_assignments = @all.where(due_date: Date.today.to_s)
+    @tomorrows_assignments = @all.where(due_date: Date.tomorrow.to_s)
+    @next_days_assignments = @all.where(due_date: Date.tomorrow.tomorrow.to_s)
+    
     @past_due = []
 
     @all.where(:completed_at => nil).map do |assignment|
@@ -10,11 +20,5 @@ class CorkboardController < ApplicationController
       end
     end
 
-    @user = current_user
-    @todays_assignments = Assignment.where(due_date: Date.today.to_s)
-    @tomorrows_assignments = Assignment.where(due_date: Date.tomorrow.to_s)
-    @next_days_assignments = Assignment.where(due_date: Date.tomorrow.tomorrow.to_s)
-    @all_tasks = Assignment.where(house_id: current_user.house.id)
-    @my_tasks = @all_tasks.where(assignee_ids: [current_user.id])
   end
 end
