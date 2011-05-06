a2e4f727c3dec603563eb9a5e66e49092e2229ad
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-06 13:31:13 -0400

    Added basic achievement types.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -8,5 +8,15 @@ class Achievement
 
   # Associations
   has_one :user
+
+  TYPE = {
+    :nice_roomie => { value: 25, badge: "badges/nice_roomie.png" },
+    :cool_roomie => { value: 50, badge: "badges/cool_roomie.png" },
+    :good_roomie => { value: 100, badge: "badges/good_roomie.png" },
+    :super_roomie => { value: 200, badge: "badges/super_roomie.png" },
+    :great_roomie => { value: 400, badge: "badges/great_roomie.png" },
+    :fantastic_roomie => { value: 800 , badge: "badges/fantastic_roomie.png" },
+    :amazing_roomie => { value: 1600, badge: "badges/amazing_roomie.png" }
+  }
   
 end
