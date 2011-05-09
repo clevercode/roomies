326b9e59f68b1a3077dd6ecc7ccc6bbbd292df14
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-09 15:21:40 -0400

    Creating "demo" house for users that don't have a house for now, we should eventually make a seed that creates a Demo house to be sure it's there and show that prefilled data as an intro to the app.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -3,7 +3,11 @@ class CorkboardController < ApplicationController
   before_filter :authenticate_user!
   def index
 
-    @all = Assignment.house(current_user.house)
+    if current_user.house
+      @all = Assignment.house(current_user.house)
+    else
+      @all = Assignment.house(House.create(name: "Demo"))
+    end
     @past_due = @all.past_due
     @my = @all.where(assignee_ids: [current_user.id])
     @tasks = @all.where(type: "task")
@@ -12,6 +16,5 @@ class CorkboardController < ApplicationController
     @todays_assignments = @all.where(due_date: Date.today.to_s)
     @tomorrows_assignments = @all.where(due_date: Date.tomorrow.to_s)
     @next_days_assignments = @all.where(due_date: Date.tomorrow.tomorrow.to_s)
-    
   end
 end
