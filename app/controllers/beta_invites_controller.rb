ffdd37b02a2dbf2c9dc643e48bdc62978d1221d8
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-12 03:48:38 -0400

    Added new and create action for BetaInvite and redirected to corkboard on success.

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -3,11 +3,18 @@ class BetaInvitesController < ApplicationController
   respond_to :html
 
   def new
-
+    @invite = BetaInvite.new
   end
 
   def create
+    @invite = BetaInvite.new(params[:beta_invite])
 
+    if @invite.save
+      flash[:notice] = t('.beta_invite_sent')
+      redirect_to corkboard_index_url
+    else
+      render :new
+    end
   end
 
 end
