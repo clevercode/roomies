c449fdadef9a66c2c71303092881e546e48efb7d
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-12 03:47:58 -0400

    Token generation with an array of silly words and beta_invite_limit decremented on creation.

--- app/models/beta_invite.rb
+++ app/models/beta_invite.rb
@@ -16,12 +16,30 @@ class BetaInvite
                               }
   validate :sender_has_invites, :if => :sender
   validate :recipient_not_registered
+
+  before_create :generate_token
+  before_create :decrement_sender_count, :if => :sender
+
   private
   def sender_has_invites
     unless sender.beta_invite_limit > 0
       errors.add_to_base t('.beta_invite_limit_reached')
     end
   end
+
+  def generate_token
+    self.token = randomly_pick(1)
+  end
+
+  def randomly_pick(number)
+    tokens = ["funky-monkey-panties", "popular-pinata-pasta", "row-row-row-your-boat"]
+    tokens.sort_by{ rand }.slice(0...number)
+  end
+
+  def decrement_sender_count
+    sender.inc(:beta_invite_limit, -1)
+  end
+
   def recipient_not_registered
     errors.add :recipient_email, 'is already signed up' if User.where(email: recipient_email).first
   end
