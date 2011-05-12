60345627a7e0da758d3b98a98692e1dd8d17272f
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-12 03:47:21 -0400

    Added validations (recipient email uniquess, presence, and format; invites left; unregistered recipient) to BetaInvite model.

--- app/models/beta_invite.rb
+++ app/models/beta_invite.rb
@@ -7,4 +7,23 @@ class BetaInvite
 
   belongs_to :sender, :class_name => 'User'
   has_one :recipient, :class_name => 'User'
+
+  validates :recipient_email, :presence => true,
+                              :uniqueness => true,
+                              :format => { 
+                                :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+                                :on => :create 
+                              }
+  validate :sender_has_invites, :if => :sender
+  validate :recipient_not_registered
+  private
+  def sender_has_invites
+    unless sender.beta_invite_limit > 0
+      errors.add_to_base t('.beta_invite_limit_reached')
+    end
+  end
+  def recipient_not_registered
+    errors.add :recipient_email, 'is already signed up' if User.where(email: recipient_email).first
+  end
+  
 end
