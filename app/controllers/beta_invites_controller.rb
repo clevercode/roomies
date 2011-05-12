220071555269f098c13b6551c2dc9c7ceb2500f4
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-12 16:04:57 -0400

    Added functionality for BetaInvite requests from eager little soon-to-be users.
    If a user is logged in, the form submits the beta invite and sends a mailer to the recipient.
    
    If not, no sender is added to the betainvite, the mailer doesn't fire, but the beta invite is logged for a future rake task, making our sinatra app useless.

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -11,9 +11,14 @@ class BetaInvitesController < ApplicationController
     @invite.sender = current_user
 
     if @invite.save
-      UserMailer.beta_invite(@invite, beta_sign_up_url(@invite.token)).deliver
-      flash[:notice] = t('.beta_invite_sent')
-      redirect_to corkboard_index_url
+      if user_signed_in?
+        UserMailer.beta_invite(@invite, beta_sign_up_url(@invite.token)).deliver
+        flash[:notice] = t('.beta_invite_sent')
+        redirect_to corkboard_index_url
+      else
+        flash[:notice] = t('.beta_invite_request_sent')
+        redirect_to root_url
+      end
     else
       render :new
     end
