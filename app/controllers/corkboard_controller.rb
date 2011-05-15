d8947071fb78042ca16bc39a414a4b34346122ad
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-15 18:34:12 -0400

    Only creating variables for the corkboard page if the current_user has a house.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -7,26 +7,26 @@ class CorkboardController < ApplicationController
       @all = nil
     else
       @all = current_user.house.assignments.where(completed_at: nil)
+      @my  = current_user.assignments.where(completed_at: nil)
+    
+      @my_confirmations          = current_user.house.assignments
+                                     .where(commissioner_id: current_user.id)
+                                     .and(:completed_at.ne => nil)
+                                     .excludes(completor_id: current_user.id)
+      
+      @my_past_due               = @my.past_due
+      @my_tasks                  = @my.where(type: "task")
+      @my_expenses               = @my.where(type: "expense")
+      @my_todays_assignments     = @my.where(due_date: Date.today.to_s)
+      @my_tomorrows_assignments  = @my.where(due_date: Date.tomorrow.to_s)
+      @my_next_days_assignments  = @my.where(due_date: Date.tomorrow.tomorrow.to_s)
+      
+      @all_tasks                 = @all.where(type: "task")
+      @all_expenses              = @all.where(type: "expense")
+      @all_todays_assignments    = @all.where(due_date: Date.today.to_s)
+      @all_tomorrows_assignments = @all.where(due_date: Date.tomorrow.to_s)
+      @all_next_days_assignments = @all.where(due_date: Date.tomorrow.tomorrow.to_s)
+      
     end
-    @my  = current_user.assignments.where(completed_at: nil)
-    
-    @my_confirmations          = current_user.house.assignments
-                                   .where(commissioner_id: current_user.id)
-                                   .and(:completed_at.ne => nil)
-                                   .excludes(completor_id: current_user.id)
-    
-    @my_past_due               = @my.past_due
-    @my_tasks                  = @my.where(type: "task")
-    @my_expenses               = @my.where(type: "expense")
-    @my_todays_assignments     = @my.where(due_date: Date.today.to_s)
-    @my_tomorrows_assignments  = @my.where(due_date: Date.tomorrow.to_s)
-    @my_next_days_assignments  = @my.where(due_date: Date.tomorrow.tomorrow.to_s)
-    
-    @all_tasks                 = @all.where(type: "task")
-    @all_expenses              = @all.where(type: "expense")
-    @all_todays_assignments    = @all.where(due_date: Date.today.to_s)
-    @all_tomorrows_assignments = @all.where(due_date: Date.tomorrow.to_s)
-    @all_next_days_assignments = @all.where(due_date: Date.tomorrow.tomorrow.to_s)
-    
-  end
+      end
 end
