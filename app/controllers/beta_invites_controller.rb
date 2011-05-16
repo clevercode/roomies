15e6818a71fd505aa7d9ad251ee08ddab70d0213
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-16 16:42:38 -0400

    Making sure a recipient doesn't already exist (email) before creating an invite.

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -8,19 +8,28 @@ class BetaInvitesController < ApplicationController
 
   def create
     @invite = BetaInvite.new(params[:beta_invite])
-    @invite.sender = current_user
 
-    if @invite.save
-      if user_signed_in?
-        UserMailer.beta_invite(@invite, beta_sign_up_url(@invite.token)).deliver
-        flash[:notice] = t('.beta_invite_sent')
-        redirect_to corkboard_index_url
+    # making sure there aren't existing invites with this recipient
+    if BetaInvite.where(recipient_email: params[:beta_invite][:recipient_email]).first.blank?
+
+      @invite.sender = current_user if user_signed_in?
+
+      if @invite.save
+        if user_signed_in?
+          UserMailer.beta_invite(@invite, beta_sign_up_url(@invite.token)).deliver
+          flash[:notice] = t('.beta_invite_sent')
+          redirect_to corkboard_index_url
+        else
+          flash[:notice] = t('.beta_invite_request_sent')
+          redirect_to root_url
+        end
       else
-        flash[:notice] = t('.beta_invite_request_sent')
-        redirect_to root_url
+        flash[:error] = "Couldn't create an beta invite, drop us a line so we fix this. Sorry."
+        render :new
       end
     else
-      render :new
+      flash[:error] = "Someone already requested an invite for this email address"
+      redirect_to "#{root_url}/?beta=true"
     end
   end
 
