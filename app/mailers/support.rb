df6f105ca934ae81092025938e034c83d81d1750
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-18 03:30:53 -0400

    Cleaned up the support request mailer output.

--- app/mailers/support.rb
+++ app/mailers/support.rb
@@ -2,19 +2,11 @@ class Support < ActionMailer::Base
   default :from => "support@roomiesapp.com"
   
   def submit_request(params)
-    request = params[:request]
+    @request = params[:request]
     mail(:to       => 'contact@clevercode.net',
-         :reply_to => request[:email],
-         :subject  => request[:message].split[0..7].join(' '),
-         :title    => "A message from #{request[:name]}:",
-         :email    => request[:email]
-    ) do |format|
-      format.text { render :text  => "#{request[:message]}" }
-      format.html { render :text  => "<h1>Hey there, you sexy beast!</h1>" +
-                                     "<h3>It seems you've gotten a new message from a loyal follower at roomies. Yay for you!</h3>" +
-                                     "===================================" +
-                                     "<p>#{request[:message]}</p>"
-      }
-    end
+         :reply_to => @request[:email],
+         :subject  => "Roomies support: " + truncate(@request[:message], :length => 10, :omission => ' (...)'),
+         :email    => @request[:email]
+    )
   end
 end
