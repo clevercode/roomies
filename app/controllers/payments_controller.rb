99cb7753b86b80ef80f63a8edb2fb197f28d39fb
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-19 06:02:42 -0400

    Skipping authenticity token since this is a direct POST with no form.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,9 @@
 class PaymentsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def handle
+    logger.info "OOOOOHHH PARAMS"
     logger.info params
+
   end
 end
