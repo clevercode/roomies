bafb94068f12a7563ef0ea868c928a3ba1456141
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-20 06:15:16 -0400

    Added jQuery equalHeights plugin.

--- public/javascripts/plugins.js
+++ public/javascripts/plugins.js
@@ -9,3 +9,21 @@ window.log = function(){
 
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+// equalHeights Plugin
+// https://gist.github.com/915094
+
+(function($){
+	$.fn.equalHeights = function() {
+		var tallest = 0,
+			current = 0;
+		this.each(function(){
+			current = $(this).height();
+			if(current > tallest) {
+				tallest = current;
+			}
+		});
+		return this.height(tallest);
+	};
+})(jQuery);
+
