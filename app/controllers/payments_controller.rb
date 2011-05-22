e6ffe2286ea5884cae7e51784a5ca2a46c949b21
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-22 03:07:46 -0400

    Added a payments controller to handle pintpay's callbacks.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,9 +1,13 @@
 class PaymentsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
+  respond_to :html, :json
+
   def handle
     logger.info "OOOOOHHH PARAMS"
     logger.info params
+    @params = params
 
+    respond_with @params
   end
 end
