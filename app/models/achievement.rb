fae5a0ffb849f928a7511347e325c544b40d3f85
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-23 22:36:01 -0400

    Displaying achievements badges that a user has received. Reduced current selection of badges to the 4 provided by Tyler.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -11,13 +11,10 @@ class Achievement
   belongs_to :user
 
   TYPES = {
-    nice_roomie:      { value: 25, badge: "badges/roomie_nice.png", category: "General" },
-    cool_roomie:      { value: 50, badge: "badges/roomie_cool.png", category: "General" },
-    good_roomie:      { value: 100, badge: "badges/roomie_good.png", category: "General" },
-    super_roomie:     { value: 200, badge: "badges/roomie_super.png", category: "General" },
-    great_roomie:     { value: 400, badge: "badges/roomie_great.png", category: "General" },
-    fantastic_roomie: { value: 800 , badge: "badges/roomie_fantastic", category: "General" },
-    amazing_roomie:   { value: 1600, badge: "badges/roomie_amazing", category: "General" }
+    good_roomie:        { value: 25, badge: "badges/roomie_good.png", category: "General" },
+    great_roomie:       { value: 50, badge: "badges/roomie_great.png", category: "General" },
+    awesome_roomie:     { value: 100, badge: "badges/roomie_awesome.png", category: "General" },
+    best_roomie:        { value: 200, badge: "badges/roomie_best.png", category: "General" }
   }
 
   attr_accessible :name, :value, :badge, :category
