d5fc417444901c31ecd36cd8dd254f8097eb840f
Olivier Lacan <olivier.lacan@gmail.com>
2011-05-24 02:35:22 -0400

    Piece of shit hash playing tough guy.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
     if @user.house
       @tasks = @user.house.assignments.where(assignee_ids: [@user.id], type: "task", completed_at: nil)
       @expenses = @user.house.assignments.where(assignee_ids: [@user.id], type: "expense", completed_at: nil)
+      @next_achievement = Achievement::TYPES.select do |k,v|
+        if v[:value] > @user.points
+          k
+        end
+      end
     end
     respond_with(@user)
   end
