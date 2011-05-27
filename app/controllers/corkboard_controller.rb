151e51d59600adaa9a0d0f70e347253ebfa57e53
Zachary Nicoll <zacharynicoll@gmail.com>
2011-05-27 00:41:25 -0400

    Changed assignments to use Date.current so all days are always in sync.

--- app/controllers/corkboard_controller.rb
+++ app/controllers/corkboard_controller.rb
@@ -11,13 +11,13 @@ class CorkboardController < ApplicationController
             
       @my_tasks                  = @my.where(type: "task")
       @my_expenses               = @my.where(type: "expense")
-      @my_todays_assignments     = @my.where(due_date: Date.today.to_s)
+      @my_todays_assignments     = @my.where(due_date: Date.current.to_s)
       @my_tomorrows_assignments  = @my.where(due_date: Date.tomorrow.to_s)
       @my_next_days_assignments  = @my.where(due_date: Date.tomorrow.tomorrow.to_s)
       
       @all_tasks                 = @all.where(type: "task")
       @all_expenses              = @all.where(type: "expense")
-      @all_todays_assignments    = @all.where(due_date: Date.today.to_s)
+      @all_todays_assignments    = @all.where(due_date: Date.current.to_s)
       @all_tomorrows_assignments = @all.where(due_date: Date.tomorrow.to_s)
       @all_next_days_assignments = @all.where(due_date: Date.tomorrow.tomorrow.to_s)
       
