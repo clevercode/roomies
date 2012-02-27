ffab43933408c7a1544e2cd35f65a5f187226545
Andrew Smith <fullsailor@me.com>
2012-02-27 16:48:08 -0500

    Remove Rails Footnotes gem.

--- config/initializers/rails_footnotes.rb
+++
@@ -1,6 +0,0 @@
-if defined?(Footnotes) && Rails.env.development?
-  Footnotes.run! # first of all
-
-  # ignore these massive objects that don't really help.
-  Footnotes::Notes::AssignsNote.ignored_assigns += [:@_env, :@_view_renderer]
-end
