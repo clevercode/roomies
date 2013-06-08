7e0f7427b508e034bf27305f4f8aedd1636eef7d
Zachary Nicoll <tower@zdn.me>
2013-06-08 02:32:21 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomies::Application.config.secret_token = '4c453aa33fc6b52c528b91754e51ef05a4ba5901aa8b2afff228dbd9b8330a31cb8baead6d7b15c50d28a848e453b4067f970a4304bedf1b2f70bc8cb162031b'
